0dc3f9a0fb7ab88f5737fc83501cb431f3e13bf6
robin mahmud <robinmahmud358@gmail.com>
2022-12-27 12:19:27 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -8,6 +9,19 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWORD_DB}@cluster0.1qyvldj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const postCollections = client.db('social').collection('posts')
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.log(err))
 
 
 
